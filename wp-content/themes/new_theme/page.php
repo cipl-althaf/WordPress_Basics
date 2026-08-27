75a89ad90dbd7c8951fecca8cfd5007c6bbb2635
cipl-althaf <cipl.althaf@gmail.com>
2026-08-27 19:49:35 +0530

    Learning Input Type (ACF)

--- wp-content/themes/new_theme/page.php
+++ wp-content/themes/new_theme/page.php
@@ -18,8 +18,64 @@
     <?php if(is_page('practice-contact-form')){
         
         echo do_shortcode('[contact-form-7 id="a7998a9" title="Practice_Contact_Form"]');
-    }
-        ?>
+        echo 'Password = '.get_field('password');
+        echo '<br>';
+        echo "Gender = ".get_field('gender'); 
+        echo "<br>";
+        echo "Button Group = ".get_field('button_group');
+        echo "<br>";
+        echo "DropDown Option = ".get_field('drop_down');
+        echo "<br>";
+        $sports = get_field('sports');
+        // print_r(get_field('sports'));
+        // $sports = get_field('sports');
+        if($sports){
+
+            echo "Favourite Sports = ".implode(', ',$sports);
+        }
+        echo "<br>";
+        echo "Gender = ".get_field('gender');
+        echo "<br>";
+        echo "Button Group = ".get_field('button_group');
+        
+    ?>
+
+    <?php if(get_field('show_section')){ ?>
+
+    <div id="section" class="border border-danger p-5">
+            <label for="">Hello</label>
+    </div>            
+
+    <?php }?>
+    <?php if(get_field('conditions')){ ?>
+    <div id="section" class="border border-danger p-5">
+            <label for="">Hello</label>
+    </div>
+    <?php }?>
+
+    <?php
+    echo "<br>";
+    
+    the_field('link');
+    echo "<br>";
+    $post_object = get_field('post_object');
+    echo "<pre>";
+    // print_r($post_object);
+    echo "</pre>";
+    echo "<br>";
+    
+
+
+$editor = get_field('new_editor');
+
+echo '<pre>';
+print_r($editor);
+echo '</pre>';
+
+
+
+    ?>
+    <?php }?>
 
     
 
@@ -33,7 +89,7 @@
         
         echo do_shortcode('[contact-form-7 id="b8986b1" title="Custom_Contact_Form2"]');
 
-         
+                
     }
     ?>
     </div>
